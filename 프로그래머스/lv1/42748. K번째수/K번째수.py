f255e93ca50977ad6934395146f4b2148eeb71da
Jiye Ryu <118228367+devdaradara@users.noreply.github.com>
2023-05-07 21:19:45 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -0,0 +1,11 @@
+def solution(array, commands):
+    answer = []
+
+    for i in range(len(commands)):
+        command = commands[i]
+        i = command[0]
+        newArr = array[command[0]-1:command[1]]
+        newArr.sort()
+        answer.append(newArr[command[2]-1])
+
+    return answer
