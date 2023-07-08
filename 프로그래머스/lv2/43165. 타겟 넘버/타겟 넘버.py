339e4519bb6aa8155e6d24ce23dc72e05124cedf
Jiye Ryu <118228367+devdaradara@users.noreply.github.com>
2023-07-08 21:18:52 +0900

    [level 2] Title: 타겟 넘버, Time: 1.37 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,3 +1,4 @@
+
 def solution(numbers, target):
     leaves = [0]
     answer = 0 
@@ -16,3 +17,25 @@ def solution(numbers, target):
             answer += 1
 
     return answer
+
+
+# import math
+
+# def solution(numbers, target):
+#     answer = 0
+
+#     all_number = [numbers[0]]
+#     all_cnt = len(numbers)
+
+#     for i in range(all_cnt):
+#         num = numbers[i]
+
+#         for j in range(int(math.pow(2, i)) - 1, int(math.pow(2, i + 1)) - 2):
+#             all_number.append(numbers[j] + num)
+#             all_number.append(numbers[j] - num)
+
+#     for k in range(int(math.pow(2, all_cnt)) - 1, len(all_number)):
+#         if all_number[k] == target:
+#             answer += 1
+
+#     return answer
