50a6f45ef8120580e6e87f6f0fe7f9700217451e
Jiye Ryu <118228367+devdaradara@users.noreply.github.com>
2023-07-16 12:28:33 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 9.93 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -0,0 +1,34 @@
+def solution(citations):
+    citations.sort()
+    for idx , citation in enumerate(citations):
+        if citation >= len(citations) - idx :
+            return len(citations) - idx
+    return 0
+
+
+# def solution(citations):
+#     answer = min(citations)
+    
+#     citations.sort()
+#     citations.insert(0, answer)
+#     candi = []
+    
+#     index_h = 0
+    
+#     for i in range(max(citations)):
+#         citations[index_h] += 1
+        
+#         for j in citations[1:]:
+#             if citations[index_h] >= j: citations[index_h] += 1
+#             else: break
+            
+#         if (len(citations) - index_h) >= citations[index_h]:    
+#             candi.append(citations[index_h])
+#         else:
+#             break
+    
+#     answer = max(candi) - 1
+    
+#     return answer
+
+# 0, 1, 1, 3, 5, 6
