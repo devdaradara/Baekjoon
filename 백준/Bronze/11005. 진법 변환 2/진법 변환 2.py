c16bf63b8e1d1e3fd4f617f4424676d1e352d0bd
Jiye Ryu <118228367+devdaradara@users.noreply.github.com>
2024-01-18 08:52:50 +0900

    [Bronze I] Title: 진법 변환 2, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -0,0 +1,20 @@
+N,B = list(map(int, input().split()))
+
+alphabet_list = list('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+num_list = list(range(36))
+
+result = ''
+
+while N >= B:
+    idx = N % B 
+
+    alphabet_idx = num_list.index(idx) 
+    result += alphabet_list[alphabet_idx] 
+
+    N = N // B
+
+
+alphabet_idx = num_list.index(N)
+result += alphabet_list[alphabet_idx]
+
+print(result[::-1])
