7882740dd3025159191f9b130b5b41b53afa0709
Jiye Ryu <118228367+devdaradara@users.noreply.github.com>
2024-02-08 19:40:47 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,34 +1,30 @@
-# import sys
-# input = sys.stdin.readline
-
+import sys
+input = sys.stdin.readline
 from collections import deque
 
 n, m = map(int, input().split())
-maze = [[0] * m for _ in range(n)]
-checked = [[False] * m for _ in range(n)]
+graph = []
 
-dx = [0, 1, 0, -1]
+for _ in range(n):
+    graph.append(list(map(int, input().rstrip())))
+
+dx = [0, -1, 0, 1]
 dy = [1, 0, -1, 0]
 
+def bfs(a, b):
+    que = deque()
+    que.append((a, b))
 
-for i in range(n):
-    kk = list(input())
-    for j in range(m):
-        maze[i][j] = int(kk[j])
+    while que:
+        x, y = que.popleft()
 
-que = deque()
-que.append((0, 0))
-checked[0][0] = True
+        for i in range(4):
+            xx = x + dx[i]
+            yy = y + dy[i]
 
-while que:
-    new = que.popleft()
-    for k in range(4):
-        x = new[0] + dx[k]
-        y = new[1] + dy[k]
-        if x >= 0 and y >= 0 and x < n and y < m:
-            if maze[x][y] != 0 and not checked[x][y]:
-                checked[x][y] = True
-                maze[x][y] = maze[new[0]][new[1]] + 1
-                que.append((x, y))
+            if 0 <= xx < n and 0 <= yy < m and graph[xx][yy] == 1:
+                que.append((xx, yy))
+                graph[xx][yy] = graph[x][y] + 1
 
-print(maze[n-1][m-1])
+bfs(0, 0)
+print(graph[n-1][m-1])
