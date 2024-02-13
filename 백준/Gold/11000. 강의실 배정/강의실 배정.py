4abbfe7ac3b8bf66e82ad032f2b4cb1fa054bc1f
Jiye Ryu <118228367+devdaradara@users.noreply.github.com>
2024-02-13 19:06:37 +0900

    [Gold V] Title: 강의실 배정, Time: 448 ms, Memory: 73448 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+from heapq import heappop, heappush
+
+n = int(input())
+times = []
+lecture = []
+
+for _ in range(n):
+    times.append(list(map(int, input().split())))
+times.sort()
+
+heappush(lecture, times[0][1])
+
+for i in range(1, n):
+    if times[i][0] < lecture[0]:
+        heappush(lecture, times[i][1])
+    else:
+        heappop(lecture)
+        heappush(lecture, times[i][1])
+        
+print(len(lecture))
